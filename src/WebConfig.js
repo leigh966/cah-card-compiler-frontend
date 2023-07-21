ffe76a8f4b22ed9b2e18493542cfcd40cec88746
leigh966 <leigh966@hotmail.com>
2023-07-21 21:16:59 +0100

    removed slashes from the end of urls

--- src/WebConfig.js
+++ src/WebConfig.js
@@ -1,3 +1,3 @@
-export const FRONTEND_URL = "http://cah-cc.itsthenikolai.com/";
+export const FRONTEND_URL = "http://cah-cc.itsthenikolai.com";
 export const BACKEND_URL =
-  "https://cah-card-compiler-b7fc15020058.herokuapp.com/";
+  "https://cah-card-compiler-b7fc15020058.herokuapp.com";
