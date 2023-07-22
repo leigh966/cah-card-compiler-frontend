53d845ede92eef06ab71238d2384a246e36b5195
leigh966 <leigh966@hotmail.com>
2023-07-22 20:59:26 +0100

    added homepage

--- src/WebConfig.js
+++ src/WebConfig.js
@@ -1,3 +1,5 @@
-export const FRONTEND_URL = "http://cah-cc.itsthenikolai.com";
+const LIVE_FRONTEND_URL = "http://cah-cc.itsthenikolai.com";
+const LOCAL_FRONTEND_URL = "http://localhost:3000";
+export const FRONTEND_URL = LIVE_FRONTEND_URL;
 export const BACKEND_URL =
   "https://cah-card-compiler-b7fc15020058.herokuapp.com";
